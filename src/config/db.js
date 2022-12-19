ebcbff176561296a174db7df2d86534aab2730ad
Joni <jcast@joni.com>
2022-12-19 02:33:58 -0300

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,3 @@
 import Sequelize from 'sequelize'
 
-export default new Sequelize('app_panel', 'postgres', '1234', {
-    host: 'localhost', 
-    dialect: 'postgres',
-    port: '5432',
-    logging: false
-})
+export default new Sequelize('postgres://app_panel_7zux_user:Dm4Thq0Cd3vKgAv4L7BUJZ8PWGAS8Hsi@dpg-cedbcbcgqg45ht8rv230-a.oregon-postgres.render.com/app_panel_7zux')
