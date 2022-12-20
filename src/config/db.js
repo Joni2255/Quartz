129fe586e7be3aac8bc16859449e48941e1dc1a4
Joni <jcast@joni.com>
2022-12-19 23:32:58 -0300

    olaadss

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,8 @@
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('app_panel_7zux', 'app_panel_7zux_user', 'Dm4Thq0Cd3vKgAv4L7BUJZ8PWGAS8Hsi', {
+export default new Sequelize('app_panel_7zux', 'app_panel_7zux_user', 'Dm4Thq0Cd3vKgAv4L7BUJZ8PWGAS8Hsi', {
     host: 'dpg-cedbcbcgqg45ht8rv230-a', 
     dialect: 'postgres',
-    protocol: 'postgres',
     port: '5432',
     dialectOptions: {},
     logging: false
